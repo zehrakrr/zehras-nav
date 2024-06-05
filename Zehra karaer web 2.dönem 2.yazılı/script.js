738f731e358ca21014fb81cba947cbe293814ed0
zehrakrr <zehrakaraer2020@gmail.com>
2024-06-05 11:04:04 +0300

    Add files via upload

---
+++ Zehra karaer web 2.dönem 2.yazılı/script.js
@@ -0,0 +1,14 @@
+let background=document.getElementById('background');
+let text=document.getElementById('text');
+let gate_left =document.getElementById('gate-left') 
+let gate_right=document.getElementById('gate-right');
+let grass=document.getElementById('grass');
+let tree_left=document.getElementById('tree-left');
+let tree_right=document.getElementById('tree-right');
+
+
+
+
+
+
+
